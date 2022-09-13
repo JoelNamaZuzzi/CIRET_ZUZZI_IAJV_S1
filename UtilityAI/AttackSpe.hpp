2f22a4cfbe3b1d94c53637899cd0160a989fd836
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2022-09-13 12:33:03 +0200

    AttackSpe classes

--- UtilityAI/AttackSpe.hpp
+++ UtilityAI/AttackSpe.hpp
@@ -1,1 +1,10 @@
 #pragma once
+#include "Action.hpp"
+#include "AttackSpeRequirement.hpp"
+class Attack : protected Action <AttackSpeRequirement> {
+private:
+	std::string name = "AttackSpe";
+public:
+	Attack(AttackSpeRequirement re, int w) :Action(re, w) {};
+	virtual bool checkPossibleAction(int MP) const { if (MP >= require.MPneeded) return true; };
+};
--- UtilityAI/AttackSpeRequirement.hpp
+++ UtilityAI/AttackSpeRequirement.hpp
@@ -1,1 +1,6 @@
 #pragma once
+#include "Requirement.hpp"
+class AttackSpeRequirement : public Requirement {
+public:
+	AttackSpeRequirement(int MP) : Requirement(MP) {};
+};
