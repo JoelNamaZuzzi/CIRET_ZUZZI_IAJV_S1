f4774d1c5a2625099ecd5c1c364f2e69fcb7b67e
Raphael <r.vejus@gmail.com>
2022-09-13 17:11:29 +0200

    fix

--- LecomteIACpp/GameManager.cpp
+++ LecomteIACpp/GameManager.cpp
@@ -1,46 +1,47 @@
 #include "GameManager.h"
 
 GameManager::GameManager() {
-	
+	initAction();
 }
 
 void GameManager::initAction() {
 	
-	AttaqueAerienne* attaqueAerienne;
+	AttaqueAerienne* attaqueAerienne = nullptr;
+	
 	attaqueAerienne->AddRequirements(new EtreEnLair());
 	attaqueAerienne->AddRequirements(new VieDeLaTete());
 	this->IaGoat.AddAction(attaqueAerienne);
 
-	AttaqueBaisser* attaqueBaisser;			
+	AttaqueBaisser* attaqueBaisser = nullptr;			
 	attaqueBaisser->AddRequirements(new EtreAccroupi());
 	attaqueBaisser->AddRequirements(new VieDesJambes());
 	this->IaGoat.AddAction(attaqueBaisser);
 
-	AttaquerDebout* attaqueDebout;
+	AttaquerDebout* attaqueDebout = nullptr;
 	attaqueDebout->AddRequirements(new EtreDebout());
 	attaqueDebout->AddRequirements(new VieDuTorse());
 	this->IaGoat.AddAction(attaqueDebout);
 
-	Baisser* seBaisser;
+	Baisser* seBaisser = nullptr;
 	seBaisser->AddRequirements(new EtreDebout());
 	this->IaGoat.AddAction(seBaisser);
 
-	Sauter* sauter;
+	Sauter* sauter = nullptr;
 	sauter->AddRequirements(new EtreDebout());
 	this->IaGoat.AddAction(sauter);
 
-	Relever* seRelever;
+	Relever* seRelever = nullptr;
 	seRelever->AddRequirements(new EtreAccroupi());
 	this->IaGoat.AddAction(seRelever);
 
-	Redescendre* redescendre;
+	Redescendre* redescendre = nullptr;
 	redescendre->AddRequirements(new EtreEnLair());
 	this->IaGoat.AddAction(redescendre);
 
-	CoupSpecial* coupSpecial;
+	CoupSpecial* coupSpecial = nullptr;
 	coupSpecial->AddRequirements(new EtreDebout());
 	coupSpecial->AddRequirements(new ATaper3Foix());
-	coupSpecial->AddRequirements(new VieDesJambes());
+	coupSpecial->AddRequirements(new VieDuTorse());
 
 }
 
