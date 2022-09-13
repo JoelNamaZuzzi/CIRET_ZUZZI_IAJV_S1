446626e731b4993a8d4eb9938e42b34e8c645147
LTShado <56226077+LTShado@users.noreply.github.com>
2022-09-13 12:52:07 +0200

    gamemanager avec la boucle gameplay

--- UtilityAI/GameManager.cpp
+++ UtilityAI/GameManager.cpp
@@ -3,4 +3,25 @@
 void GameManager::Init() {
 	WhoStart start;
 	Turn turn;
+	Player player;
+	Player AI;
+
+	GameState();
+}
+
+void GameManager::GameState() {
+	bool gamestate = true;
+	while (gamestate)
+	{
+		if (start.WhoStartRandomiser() == 1) {
+			cout << "Youre 1st \n";
+			turn.PlayerTurn(1);
+			gamestate = false;
+		}
+		else {
+			cout << "Youre 2nd \n";
+			turn.PlayerTurn(0);
+			gamestate = false;
+		}
+	}
 }
--- UtilityAI/GameManager.h
+++ UtilityAI/GameManager.h
@@ -5,7 +5,11 @@ class GameManager
 public:
 	void Init();
 	WhoStart start;
+	Player player;
+	Player AI;
 	Turn turn;
+
+	void GameState();
 
 private:
 
