e50cb1694e71f6cc4120d1de37ac914c139146b7
LTShado <56226077+LTShado@users.noreply.github.com>
2022-09-16 23:14:40 +0200

    fight meca

--- UtilityAI/Fight.cpp
+++ UtilityAI/Fight.cpp
@@ -1,6 +1,11 @@
 #include "Fight.h"
-#include "GameManager.h"
+#include "HeaderActions.hpp"
 
-void Fight::FightAgainst(Player p1, Player p2, int damage) {
-	p2.HP = 42;
+void Fight::FightAgainst(Player* p1, Player* p2, int action) {
+	p2->HP -= action;
+	if (action == 1) {}
+	if (action == 2) {}
+	if (action == 3) {}
+	if (action == 4) {}
+	if (action == 5) {}
 }
--- UtilityAI/Turn.cpp
+++ UtilityAI/Turn.cpp
@@ -4,45 +4,55 @@ using namespace std;
 #include "GameManager.h"
 
 void Turn::PlayerTurn(int t) {
-	int wait;
+	int action;
 	GameManager gm;
-	Player player;
-	AI playerAI;
+	Player* player = new Player;
+	AI* playerAI = new AI;
 
-	while (player.HP > 0 && playerAI.HP) {
+	while (player->HP > 0 && playerAI->HP) {
 		if (t == 1) {
 			if (counterTurn % 2 == 1) {
 				cout << "Your turn : " << "\n";
-				cin >> wait;
-				gm.fight.FightAgainst(player, playerAI, wait);
-				cout << player.HP << "\n";
-				cout << playerAI.HP << "\n";
+				cout << "1 -> Attack" << "\n";
+				cout << "2 -> AttackSpe" << "\n";
+				cout << "3 -> Buff" << "\n";
+				cout << "4 -> Debuff" << "\n";
+				cout << "5 -> Defense" << "\n";
+				cin >> action;
+				gm.fight.FightAgainst(player, playerAI, action);
+				cout << player->HP << "\n";
+				cout << playerAI->HP << "\n";
 				counterTurn++;
 			}
 			else {
 				cout << "Wait for your oppenent : " << "\n";
-				cin >> wait;
-				gm.fight.FightAgainst(playerAI, player, wait);
-				cout << player.HP << "\n";
-				cout << playerAI.HP << "\n";
+				cin >> action;
+				gm.fight.FightAgainst(playerAI, player, action);
+				cout << player->HP << "\n";
+				cout << playerAI->HP << "\n";
 				counterTurn++;
 			}
 		}
 		else {
 			if (counterTurn % 2 == 0) {
 				cout << "Your turn : " << "\n";
-				cin >> wait;
-				gm.fight.FightAgainst(player, playerAI, wait);
-				cout << player.HP << "\n";
-				cout << playerAI.HP << "\n";
+				cout << "1 -> Attack" << "\n";
+				cout << "2 -> AttackSpe" << "\n";
+				cout << "3 -> Buff" << "\n";
+				cout << "4 -> Debuff" << "\n";
+				cout << "5 -> Defense" << "\n";
+				cin >> action;
+				gm.fight.FightAgainst(player, playerAI, action);
+				cout << player->HP << "\n";
+				cout << playerAI->HP << "\n";
 				counterTurn++;
 			}
 			else {
 				cout << "Wait for your oppenent : " << "\n";
-				cin >> wait;
-				gm.fight.FightAgainst(playerAI, player, wait);
-				cout << player.HP << "\n";
-				cout << playerAI.HP << "\n";
+				cin >> action;
+				gm.fight.FightAgainst(playerAI, player, action);
+				cout << player->HP << "\n";
+				cout << playerAI->HP << "\n";
 				counterTurn++;
 			}
 		}
